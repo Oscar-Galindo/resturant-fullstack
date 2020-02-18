60eae6e616fce13c36646161b5c573dd255b8c89
Oscar-Galindo <onmgalindo@gmail.com>
2020-02-18 00:32:12 -0500

    Added About Us Page. Added styles and content to page.

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,7 +5,24 @@
 
 
 @section('content')
-   <div class="welcome-jumbo">
-      <h1>About Page</h1>
-    </div>
+  <div id="about-page">
+     <div class="content-box">
+      <div class="row">
+         <div class="col-md-6">
+         <h1>About Page</h1>
+         <p>
+            Lorem ipsum dolor amet squid shabby chic cray, beard hell of poutine microdosing messenger bag. Health goth actually artisan bicycle rights franzen, marfa austin sustainable ennui live-edge man braid beard deep v raclette. Polaroid asymmetrical 3 wolf moon knausgaard, pabst poke single-origin coffee pug edison bulb venmo kogi wolf vinyl tbh. Neutra fanny pack glossier man braid fam narwhal whatever mixtape skateboard. Next level food truck activated charcoal snackwave schlitz before they sold out. Venmo raclette mixtape pork belly post-ironic palo santo taxidermy, skateboard kitsch gentrify ennui. Freegan roof party normcore brooklyn twee hammock fanny pack tofu.
+         </p>
+         
+         <p
+         >Lorem ipsum dolor amet squid shabby chic cray, beard hell of poutine microdosing messenger bag. Health goth actually artisan bicycle rights franzen, marfa austin sustainable ennui live-edge man braid beard deep v raclette. Polaroid asymmetrical 3 wolf moon knausgaard, pabst poke single-origin coffee pug edison bulb venmo kogi wolf vinyl tbh. Neutra fanny pack glossier man braid fam narwhal whatever mixtape skateboard. Next level food truck activated charcoal snackwave schlitz before they sold out. Venmo raclette mixtape pork belly post-ironic palo santo taxidermy, skateboard kitsch gentrify ennui. Freegan roof party normcore brooklyn twee hammock fanny pack tofu.
+         </p>
+      </div>
+    
+     <div class="col-md-6">
+        <img src="/img/about-coffee.jpeg" alt="coffee-time" class="img-fluid">
+     </div>
+     </div>
+   </div> 
+  </div>
 @endsection
