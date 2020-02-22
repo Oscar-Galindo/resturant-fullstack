bb08e0ecc9543ffe69d282f1e5e20415ce2b423e
Oscar-Galindo <onmgalindo@gmail.com>
2020-02-22 01:31:11 -0500

    Set Up Admin Layout, Added Login and Register Pages, and Edited The Admin Side Menu.

---
+++ resources/views/menu/single-menu.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+
+
+
+
+
+@section('content')
+  <div id="single-menu-page">
+      <div class="row">
+          <div class="offset-sm-12 offset-md-1 col-md-10 offset-lg-2 col-lg-8">
+            <div class="content-box">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h1>Burgers</h1>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="item">
+                                <div class="title">
+                                    <h4>Texas Burger</h4>
+                                    <span class="price">$9</span>
+                                </div>
+                                <div class="description">
+                                    <p>Scrumptious, 100% beef patty with zero additives or preservatives, seasoned with pepper and salt, and topped with a slice of cheese, lettuce, tomato, onion, pickles, and a condiment of your choice on a freshly baked bun.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="item">
+                                <div class="title">
+                                    <h4>Bacon Cheese Burger</h4>
+                                    <span class="price">$12</span>
+                                </div>
+                                <div class="description">
+                                    <p>Crisp, thick-cut & 100% halal mock bacon tops off an 100% beef preservative and additive free beef patty, adorned with melty cheese, crisp lettuce, juicy tomato, onions pickles, and your choice of sauce on a freshly baked bun.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="item">
+                                <div class="title">
+                                     <h4>Texas Double Cheese Burger</h4>
+                                    <span class="price">$15</span>
+                                </div>
+                                <div class="description">
+                                    <p>2 sumptuous, 100% beef patties with zero additives or preservatives, seasoned with pepper and salt, and topped with a slice of cheese, lettuce, tomato, onion, pickles, and a condiment of your choice on a freshly baked bun.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="item">
+                                <div class="title">
+                                    <h4>Texas Original Fire Burger</h4>
+                                    <span class="price">$15</span>
+                                </div>
+                                <div class="description">
+                                    <p>Our signature, 100% beef patty with zero additives or preservatives, seasoned with salt and pepper, with fried onions, jalapeños, a slice of Swiss cheese, and a slice of American Cheese on a freshly baked bun, drizzled with our fiery Sauce for a truly heated experience.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+          </div>
+      </div>
+  </div>
+@endsection
